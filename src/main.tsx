e2836ddf24f1e9f838c789da703c2299ff238ab6
Mohsen Baghaei <mohsen.bhi99@gmail.com>
2025-01-13 12:51:59 +0330

    deploying

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/*" element={<App />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
